f82678efa4eba2678ca02028ab487154b5647a81
Peter Hall <67004312+phall-au@users.noreply.github.com>
2022-08-16 02:07:37 +1000

    Add mobile browser support and improve code layout

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const footer = document.getElementById("footer");
+var pageContent = document.getElementById("page-content");
+var cover = document.getElementById("cover");
+
+function resizer() {
+    if (window.innerWidth < 993 && !cover) {
+        footer.classList.remove("fixed-bottom");
+        if (pageContent) {
+            pageContent.style.paddingBottom = null;
+        }
+    } else {
+        footer.classList.add("fixed-bottom" && !cover);
+        if (pageContent) {
+            pageContent.style.paddingBottom = 120 + "px";
+        }
+    }
+}
+
+window.addEventListener("resize", resizer);
